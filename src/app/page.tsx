7cc81b38742f785927bb64fce39bc842f198ab84
Hibiki-moreno <hibikicarlo@gmail.com>
2024-11-14 19:54:49 -0600

    Dock

---
+++ src/app/page.tsx
@@ -0,0 +1,19 @@
+import BlurFade from "@/components/magicui/blur-fade";
+import BlurFadeText from "@/components/magicui/blur-fade-text";
+import { ProjectCard } from "@/components/project-card";
+import { ResumeCard } from "@/components/resume-card";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Badge } from "@/components/ui/badge";
+import { DATA } from "@/data/resume";
+import Link from "next/link";
+import Markdown from "react-markdown";
+
+const BLUR_FADE_DELAY = 0.04;
+
+export default function Page() {
+  return (
+    <main className="flex flex-col min-h-[100dvh] space-y-10">
+     <p>hola</p>
+    </main>
+  );
+}
